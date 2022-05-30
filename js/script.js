9bb36758fe066956c3a273d7e1de6099b9d89116
Anych <anuar123@mail.ru>
2022-05-30 20:35:35 +0600

    Rewrite fifth hw

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
 const movieDB = {
@@ -27,7 +13,7 @@ const movieDB = {
 const adv = document.querySelectorAll('.promo__adv img'),
       poster = document.querySelector('.promo__bg'),
       genre = poster.querySelector('.promo__genre'),
-      movieList = document.querySelectorAll('.promo__interactive-list');
+      movieList = document.querySelector('.promo__interactive-list');
 
 adv.forEach(item => {
     item.remove();  
@@ -38,6 +24,8 @@ genre.textContent = 'драма';
 poster.style.backgroundImage = 'url("img/bg.jpg")';
 
 movieDB.movies.sort();
+
+movieList.innerHTML = '';
 
 movieDB.movies.forEach((film, i) => {
     movieList.innerHTML += `
